2069a595fbbba85177e04cc951c8bd4b1697eb4c
Johan Olsson <jawstiunda@gmail.com>
2016-06-19 20:00:37 +0200

    added photos

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # RPi-photobooth
 Camera and photo printer controlled by a raspberry pi
 
+## Images
+![GitHub Logo](/photobooth_front.png)
+Format: ![Alt Text](url)
+
+![GitHub Logo](/photobooth_back.png)
+Format: ![Alt Text](url)
+
 Sometimes printer hangs (out of paper, for example): Printer Canon_CP910 disabled since Tue 17 May 2016 21:26:12 UTC -
 	Unable to send data to printer.
 
--- button.py
+++ button.py
@@ -14,37 +14,42 @@ GPIO.setup(pose_led, GPIO.OUT)
 
 GPIO.output(ready_led, False)
 GPIO.output(busy_led, 0)
+GPIO.output(pose_led, 0)
 try:
     GPIO.output(ready_led, 1)
     while (True):
-        print(GPIO.input(switch))
+        #print(GPIO.input(switch))
         if (not GPIO.input(switch)):
-            os.system("sudo service cups restart")
-            GPIO.output(ready_led, 0)
-            GPIO.output(pose_led, 1)
-            print("button pushed, sleep 5")
-            #time.sleep(3)
-            for i in range(5):
-                GPIO.output(pose_led, 0)
-                time.sleep(0.4)
+            tick = 0
+            while tick < 4:
+                os.system("sudo service cups restart")
+                GPIO.output(ready_led, 0)
                 GPIO.output(pose_led, 1)
-                time.sleep(0.4)
-            for j in range(5):
+                print("button pushed, take 4 photos")
+                for i in range(5):
+                    GPIO.output(pose_led, 0)
+                    time.sleep(0.4)
+                    GPIO.output(pose_led, 1)
+                    time.sleep(0.4)
+                for j in range(5):
+                    GPIO.output(pose_led, 0)
+                    time.sleep(0.1)
+                    GPIO.output(pose_led, 1)
+                    time.sleep(0.1)
+                print("CLICK!")
+                #gpout = "OK"
+                gpout = subprocess.check_output("gphoto2 --capture-image-and-download --filename /home/pi/photobooth/foto%H%M%S.jpg", stderr=subprocess.STDOUT, shell=True)
+                print(gpout)
+                if "ERROR" not in gpout:
+                    tick += 1
                 GPIO.output(pose_led, 0)
-                time.sleep(0.1)
-                GPIO.output(pose_led, 1)
-                time.sleep(0.1)
-            GPIO.output(pose_led, 0)
-            time.sleep(3)
+                time.sleep(1.5)
             GPIO.output(busy_led, 1)
-            gpout = subprocess.check_output("gphoto2 --capture-image-and-download --filename/home/pi/photobooth/foto.jpg", stderr=subprocess.STDOUT, shell=True)
-            print(gpout)
-            time.sleep(1.5)
             print("about to call print_photo script...")
             subprocess.call("sudo /home/pi/photobooth/print_photo", shell=True)
-            time.sleep(75)
+            time.sleep(50)
             subprocess.call("sudo /home/pi/photobooth/restart_printer", shell=True)
-            time.sleep(15)
+            time.sleep(10)
             GPIO.output(busy_led, 0)
             GPIO.output(ready_led, 1)
             GPIO.output(pose_led, 0)
